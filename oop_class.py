82e9c28adde972a8946baded725ef1a02c6945dc
joecy765 <joecy765@gmail.com>
2025-05-10 10:17:51 +0100

    my work

---
+++ oop_class.py
@@ -0,0 +1,69 @@
+# x = # Global variable
+# def addition(): # function created
+#     y = 14 
+#     z = x + y
+#     return print(z)
+
+# defining class
+# class Smartphone:
+#     # constructor
+#     def _init_(self, device, brand):
+#         self.device = device
+#         self.brand = brand
+        
+# method of the classs
+# def description(self):
+#     return f"{self.device} of {self.brand} supports android 14"
+
+# creating object of the class
+# phoneObj = Smartphone("Smartphone", "Samsung")
+# print(phoneObj,description())
+
+# creation object of the class
+# phoneObj = samsungphone("smartphone", "samsung")
+# print(phoneObj.description())
+# phone_obj = smartphone("smartphone", "samsung")
+# print(phone_obj.):
+
+# creating a class my fullname:
+# class Name:
+    # def __init__(self, first_name, middle_name, last_name):
+        # self.f_name = first_name
+        # self.m_name = middle_name
+        # self.l_name = last_name
+    # def fullname(self):
+        # return print(f"my name is {self.f_name} {self.m_name} {self.l_name}")
+#my_fullname("joseph," "nathaniel" "ekeke")
+# Fullname = Name("joseph", "nathaniel", "ekeke")
+
+
+# class class_name:
+#     x = "data engineering"
+# course = class_name()
+# print(course.x)
+
+
+#creating a class name
+# class person:
+#     def __init__(self, name, age):
+#         self.name = Name
+#         self.age = age
+#     def __str__(self)
+#         return f(self.name),
+# p = person("joseph", 14)
+# print(p.name)
+# print(p.age)
+# print(p)
+
+
+# class car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#     def display_info(self):
+#         return print(f"{self.make} {self.model} {self.year}")
+# car_detail = car("toyota", "avellon", "2025")
+# car_detail.display_info()
+    
+       
