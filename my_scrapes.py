98c25172e84c2aeff72a31117145e30ca88e60be
joecy765 <joecy765@gmail.com>
2025-05-10 10:21:11 +0100

    my works

---
+++ my_scrapes.py
@@ -0,0 +1,11 @@
+
+# import requests
+# from bs4 import BeautifulSoup as bs
+
+
+# git_user = input('Enter the github username: ')
+# ur1 = 'https://github.com/' + git_user
+# r = requests.get(ur1)
+# soup = bs(r.content, 'html.parser')
+# profile_image = soup.find('img', {'class': 'avater'})['src']
+# print(profile_image)
